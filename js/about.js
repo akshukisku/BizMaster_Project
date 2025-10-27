613f15fa22039ceee327694f9a9ff9b2e5a9df97
akshukisku <akshay.kisku4@gmail.com>
2025-10-27 19:30:50 +0530

    responsive done on mian page

---
+++ js/about.js
@@ -0,0 +1,69 @@
+const swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.custom-next',
+    prevEl: '.custom-prev',
+  },
+  on: {
+    slideChange: function () {
+      const progress = document.querySelector('.progress');
+      const totalSlides = this.slides.length - this.params.slidesPerView + 1;
+      const currentIndex = this.activeIndex + 1;
+      const percentage = (currentIndex / totalSlides) * 100;
+      progress.style.width = percentage + '%';
+    },
+  },
+});
+$(document).ready(function () {
+  const $menuToggle = $("#menuToggle");
+  const $navbarItems = $("#navbarItems");
+  const $icon = $menuToggle.find("i");
+
+  // Toggle menu on click
+  $menuToggle.on("click", function () {
+    $navbarItems.toggleClass("active");
+
+    if ($navbarItems.hasClass("active")) {
+      $icon.removeClass("ri-menu-3-line").addClass("ri-close-line");
+    } else {
+      $icon.removeClass("ri-close-line").addClass("ri-menu-3-line");
+    }
+  });
+
+  // Close menu when clicking a nav link (on small screens)
+  $(".nav-list a").on("click", function () {
+    if ($(window).width() <= 991) {
+      $navbarItems.removeClass("active");
+      $icon.removeClass("ri-close-line").addClass("ri-menu-3-line");
+    }
+  });
+
+  // Close menu when window is resized above 991px
+  $(window).on("resize", function () {
+    if ($(window).width() > 991) {
+      $navbarItems.removeClass("active");
+      $icon.removeClass("ri-close-line").addClass("ri-menu-3-line");
+    }
+  });
+  
+  let stickyNavTop = $(".navbar").offset().top;
+  console.log(stickyNavTop)
+  function stickyNav() {
+    let scrollTop = $(window).scrollTop();
+    console.log(scrollTop)
+
+    if (scrollTop > stickyNavTop) {
+      $(".navbar").addClass("sticky");
+      console.log("added");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  }
+
+  stickyNav();
+
+  $(window).on("scroll", function () {
+    stickyNav();
+  });
+});
--- js/contact.js
+++ js/contact.js
@@ -51,4 +51,57 @@ $(document).ready(function() {
                     $("#emailErr").html("");
                 }
             }
+
+
+  const $menuToggle = $("#menuToggle");
+  const $navbarItems = $("#navbarItems");
+  const $icon = $menuToggle.find("i");
+
+  // Toggle menu on click
+  $menuToggle.on("click", function () {
+    $navbarItems.toggleClass("active");
+
+    if ($navbarItems.hasClass("active")) {
+      $icon.removeClass("ri-menu-3-line").addClass("ri-close-line");
+    } else {
+      $icon.removeClass("ri-close-line").addClass("ri-menu-3-line");
+    }
+  });
+
+  // Close menu when clicking a nav link (on small screens)
+  $(".nav-list a").on("click", function () {
+    if ($(window).width() <= 991) {
+      $navbarItems.removeClass("active");
+      $icon.removeClass("ri-close-line").addClass("ri-menu-3-line");
+    }
+  });
+
+  // Close menu when window is resized above 991px
+  $(window).on("resize", function () {
+    if ($(window).width() > 991) {
+      $navbarItems.removeClass("active");
+      $icon.removeClass("ri-close-line").addClass("ri-menu-3-line");
+    }
+  });
+  
+  let stickyNavTop = $(".navbar").offset().top;
+  console.log(stickyNavTop)
+  function stickyNav() {
+    let scrollTop = $(window).scrollTop();
+    console.log(scrollTop)
+
+    if (scrollTop > stickyNavTop) {
+      $(".navbar").addClass("sticky");
+      console.log("added");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  }
+
+  stickyNav();
+
+  $(window).on("scroll", function () {
+    stickyNav();
+  });
 });
+
